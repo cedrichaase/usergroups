71866b95ad3357ecf46fb4eee4b06b8e228bbeb2
Cedric Haase <cedric@haase-online.com>
2017-12-17 01:33:29 +0100

    Add return value

--- init.lua
+++ init.lua
@@ -18,14 +18,24 @@ function usergroups:save()
 end
 
 
-
 minetest.register_chatcommand("usergroups_add", {
     params = "<group> <user>",
     description = "Add a user to a group",
     privs = {["server"] = true},
     func = function(name, param)
         local group, user = param:match('^(%S)%s(%S)$')
+
+        if not group then
+            return false, "No group specified"
+        end
+
+        if not user then
+            return false, "No user specified"
+        end
+
         groups = {[group] = {[user]=true}}
         usergroups:save()
+
+        return true, "User groups updated."
     end
 })
