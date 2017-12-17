6bfeaa844cc6fe0082b7eb74221e51c34fbf2fa0
Cedric Haase <cedric@haase-online.com>
2017-12-17 01:31:16 +0100

    Fix some stuff

--- init.lua
+++ init.lua
@@ -1,12 +1,13 @@
 usergroups = {}
+groups = {}
+
 
 usergroups.modpath = minetest.get_modpath("usergroups")
 
 filepath = minetest.get_worldpath().."/usergroups.dat"
 
 function usergroups:save()
-    local data = {foo = "bar"}
-    datastr = minetest.serialize(data)
+    datastr = minetest.serialize(groups)
 
     local file, err = io.open(filepath, "w")
     if err then
@@ -16,15 +17,15 @@ function usergroups:save()
     file:close()
 end
 
+
+
 minetest.register_chatcommand("usergroups_add", {
     params = "<group> <user>",
     description = "Add a user to a group",
     privs = {["server"] = true},
     func = function(name, param)
-
-
         local group, user = param:match('^(%S)%s(%S)$')
-        usergroups = {[group] = {[user]=true}}
+        groups = {[group] = {[user]=true}}
         usergroups:save()
     end
 })
